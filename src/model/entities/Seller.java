20fae504c5937c376564ad643e2110597661245a
gabriel scariot <gabriel.s.ferreira.rtn@gmail.com>
2025-10-08 18:13:20 -0300

    Seller, classe de vendendores

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,92 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
+	private String name;
+	private Date birthDate;
+	private Double baseSalary;
+
+	private Departament departament;
+
+	public Seller() {
+
+	}
+
+	public Seller(Integer id, String name, Date birthDate, Double baseSalary, Department departament) {
+
+		this.id = id;
+		this.name = name;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.departament = departament;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Departament getDepartament() {
+		return departament;
+	}
+
+	public void setDepartament(Departament departament) {
+		this.departament = departament;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", birthDate=" + birthDate + ", baseSalary=" + baseSalary + "]";
+	}
+}
